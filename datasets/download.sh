ad909476da1ec1c75d7a1397fe98c364e68e46d8
Nutchanon Ninyawee <nutchanon@codustry.com>
2023-03-17 02:05:36 +0700

    ‼️ BREAKING: add WIP shell cmd to download

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,3 +1,4 @@
 # grab latest data here in the artifacts of the backup workflow
 # https://github.com/creatorsgarten/bkkchangelog/actions/workflows/backup.yml
 # and save it to the datasets folder
+# gh api repos/creatorsgarten/bkkchangelog/actions/artifacts --jq '.artifacts | sort_by(.created_at) | last | .archive_download_url' | xargs curl -L -o bkkchangelog.zip
